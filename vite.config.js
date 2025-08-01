9842ad82576aaccd214d9fafa49378b5542a18ce
HVPillai <104548876+HVPillai@users.noreply.github.com>
2025-08-02 00:14:57 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/plantique-website/', // add this for GitHub Pages
-  plugins: [react()],
+  base: '/plantique-website/', // for GitHub Pages
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
